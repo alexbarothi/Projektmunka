b86a9dae8822277258bc8c003d6544585494976f
alexbarothi <alexbarothi@gmail.com>
2026-04-05 00:23:11 +0200

    LoginPage codebehind TryLogin function created

--- CritiqlyNexusCore/LoginPage.xaml.cs
+++ CritiqlyNexusCore/LoginPage.xaml.cs
@@ -1,9 +1,17 @@
+using System.Text;
+using System.Text.Json;
+
 namespace CritiqlyNexusCore;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    public async void TryLogin(object sender, EventArgs e)
+    {
+
+    }
 }
