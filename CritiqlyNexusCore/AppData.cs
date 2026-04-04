e8c09280c1d3c5ea935b2736416d17ffea704277
alexbarothi <alexbarothi@gmail.com>
2026-04-05 00:37:02 +0200

    AppData static class created

---
+++ CritiqlyNexusCore/AppData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CritiqlyNexusCore
+{
+    public static class AppData
+    {
+        public static string Username { get; set; }
+    }
+}
