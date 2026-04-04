f40f2cdb7f371588a41e5b63229a2543709a6f7f
alexbarothi <alexbarothi@gmail.com>
2026-04-05 00:34:12 +0200

    User model created

---
+++ CritiqlyNexusCore/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CritiqlyNexusCore.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public DateTime? email_verified_at { get; set; }
+        public string password { get; set; }
+        public bool is_admin { get; set; }
+
+    }
+}
