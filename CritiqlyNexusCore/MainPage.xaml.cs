c2caeb19572fc52e9a837ca15d11cc2f76c9502e
alexbarothi <alexbarothi@gmail.com>
2026-04-05 15:03:38 +0200

    Added AdminData Model and GET request for it in MainPage

--- CritiqlyNexusCore/MainPage.xaml.cs
+++ CritiqlyNexusCore/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Platform;
+﻿using System.Text.Json;
+using CritiqlyNexusCore.Models;
+using Microsoft.Maui.Platform;
 
 namespace CritiqlyNexusCore
 {
@@ -18,6 +20,26 @@ namespace CritiqlyNexusCore
             welcomeLabel.Text = "Üdv újra, " + AppData.Username + "!";
 
             GetMovies();
+            GetAdminData();
+        }
+
+        public async void GetAdminData()
+        {
+            var client = new HttpClient();
+
+            var data = await client.GetAsync("http://127.0.0.1:8000/api/admin/get");
+            var json = await data.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<AdminData>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            await DisplayAlertAsync("DEBUG", result[0].DailyLastUpdate + " - " + result[0].TrendingLastUpdate, "OK");
+
+            AppData.DailyLastUpdated = result[0].DailyLastUpdate;
+            AppData.TrendingLastUpdated = result[0].TrendingLastUpdate;
+
         }
 
         public async void GetMovies()
