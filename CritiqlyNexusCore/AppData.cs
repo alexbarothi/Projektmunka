6a75031a9c80fc4bdd4d4ab57224db5cf0812c2e
alexbarothi <alexbarothi@gmail.com>
2026-04-05 14:53:20 +0200

    Added Daily and Trending LastUpdated variable to AppData

--- CritiqlyNexusCore/AppData.cs
+++ CritiqlyNexusCore/AppData.cs
@@ -7,5 +7,7 @@ namespace CritiqlyNexusCore
     public static class AppData
     {
         public static string Username { get; set; }
+        public static DateTime? DailyLastUpdated { get; set; }
+        public static DateTime? TrendingLastUpdated { get; set; }
     }
 }
