681134a8b77f7868a67377a9d5faf6ebdc261752
alexbarothi <alexbarothi@gmail.com>
2026-04-07 12:04:36 +0200

    Added skeleton for DailyPage codebehind

--- CritiqlyNexusCore/DailyPage.xaml.cs
+++ CritiqlyNexusCore/DailyPage.xaml.cs
@@ -1,9 +1,61 @@
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using CritiqlyNexusCore.Models;
+
 namespace CritiqlyNexusCore;
 
 public partial class DailyPage : ContentPage
 {
-	public DailyPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Movie> QueryMovies { get; set; } = new ObservableCollection<Movie>();
+    public List<DailyMovie> DailyMovies = new List<DailyMovie>();
+    public List<int> CurrentDayIds = new();
+
+    public DailyPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+
+    public async void SearchQuery(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void RollRandom(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void AddToDailyList(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void checkSelected(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void getDailys()
+    {
+
+    }
+
+    public async void LoadDailys(object sender, DateChangedEventArgs e)
+    {
+
+    }
+    public async void Exit(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void Save(Object sender, EventArgs e)
+    {
+
+    }
 }
