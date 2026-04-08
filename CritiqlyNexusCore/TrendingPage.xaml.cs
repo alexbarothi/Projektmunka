2d278c8cafe0709f1c9c7fea6eef9de0c5720e96
alexbarothi <alexbarothi@gmail.com>
2026-04-08 19:08:14 +0200

    Added checkseleted to trendingpage

--- CritiqlyNexusCore/TrendingPage.xaml.cs
+++ CritiqlyNexusCore/TrendingPage.xaml.cs
@@ -92,7 +92,18 @@ public partial class TrendingPage : ContentPage
 
     public async void checkSelected(Object sender, EventArgs e)
     {
+        QueryMovies.Clear();
+        checkSelectedBtn.BackgroundColor = Colors.Orange;
 
+        foreach (var movie in AppData.Movies)
+        {
+            if (SelectedIds.Contains(movie.id))
+            {
+                QueryMovies.Add(movie);
+            }
+        }
+        await Task.Delay(500);
+        checkSelectedBtn.BackgroundColor = Color.FromRgb(212, 255, 62);
     }
 
     public async void Exit(Object sender, EventArgs e)
