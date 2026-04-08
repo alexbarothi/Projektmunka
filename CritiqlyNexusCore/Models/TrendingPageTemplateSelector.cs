e466826b1adede22459b7cacb6e05fc5be96b570
alexbarothi <alexbarothi@gmail.com>
2026-04-08 12:10:43 +0200

    TemplateSelector model and content resorces set for TrendingPage

---
+++ CritiqlyNexusCore/Models/TrendingPageTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CritiqlyNexusCore.Models
+{
+    public class DailyPageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate SelectedTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var movie = item as Movie;
+
+            if (movie.isSelectedDaily)
+                return SelectedTemplate;
+
+            return NormalTemplate;
+        }
+    }
+}
