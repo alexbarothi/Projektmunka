c853b8ff8f1c128ead24470697fdb5af20fde27c
alexbarothi <alexbarothi@gmail.com>
2026-04-08 13:27:56 +0200

    TrendingPage added: UI, Codebehind skeleton

--- CritiqlyNexusCore/TrendingPage.xaml.cs
+++ CritiqlyNexusCore/TrendingPage.xaml.cs
@@ -1,9 +1,64 @@
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using CritiqlyNexusCore.Models;
+
 namespace CritiqlyNexusCore;
 
 public partial class TrendingPage : ContentPage
 {
-	public TrendingPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Movie> QueryMovies { get; set; } = new ObservableCollection<Movie>();
+    public List<int> SelectedIds = new List<int>();
+    public TrendingPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
+
+    public async void SearchQuery(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void autoSelect(Object sender, EventArgs e)
+    {
+
+    }
+    public async void AddToTrendingList(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void checkSelected(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void Exit(Object sender, EventArgs e)
+    {
+        var isResponseOk = await DisplayAlertAsync(
+            "Kilépés",
+            "Biztosan ki akarsz lépni? Az eddigi változtatások elvesznek!",
+            "Igen",
+            "Mégse"
+        );
+
+        if (isResponseOk)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public async void Save(Object sender, EventArgs e)
+    {
+
+    }
 }
