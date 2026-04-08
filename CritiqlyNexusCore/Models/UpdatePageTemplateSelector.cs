f379fc82f73f4bb58eb7cf1bf4f3787b7a5e5342
alexbarothi <alexbarothi@gmail.com>
2026-04-08 22:46:23 +0200

    Created UpdatePageTemplateSelector

---
+++ CritiqlyNexusCore/Models/UpdatePageTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CritiqlyNexusCore.Models
+{
+    public class UpdatePageSelector : DataTemplateSelector
+    {
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate SelectedTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var movie = item as Movie;
+
+            if (movie.IsUpdated)
+                return SelectedTemplate;
+
+            return NormalTemplate;
+        }
+    }
+}
