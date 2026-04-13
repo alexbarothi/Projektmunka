a58aaf2a616982917f781c2f63476e8ffb13b3a7
alexbarothi <alexbarothi@gmail.com>
2026-04-13 12:26:04 +0200

    added onLoad function to UpdateSubPage

--- CritiqlyNexusCore/UpdateSubPage.xaml.cs
+++ CritiqlyNexusCore/UpdateSubPage.xaml.cs
@@ -6,4 +6,22 @@ public partial class UpdateSubPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        LabelTitle.Text = AppData.UpdatePageSelectedMovie.title;
+        LabelGenre.Text = AppData.UpdatePageSelectedMovie.genre;
+        LabelPlot.Text = AppData.UpdatePageSelectedMovie.plot.Replace(".", "." + System.Environment.NewLine);
+        //await DisplayAlertAsync("DEBUG", AppData.updatePageSelectedMovie.releaseDate.ToString(), "OK");
+        LabelDate.Text = AppData.UpdatePageSelectedMovie.releaseDate?.ToString("yyyy-MM-dd");
+        LabelPoster.Text = AppData.UpdatePageSelectedMovie.poster;
+
+        EntryTitle.Text = "";
+        EntryGenre.Text = "";
+        EntryPlot.Text = "";
+        EntryDate.Text = "";
+        EntryPoster.Text = "";
+    }
 }
