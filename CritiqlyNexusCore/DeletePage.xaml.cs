5153a1b80502a14d9f33541f5f6c1d5a31512ce9
alexbarothi <alexbarothi@gmail.com>
2026-04-14 14:35:51 +0200

    OnAppearing and Searchquery function added to DeletePage

--- CritiqlyNexusCore/DeletePage.xaml.cs
+++ CritiqlyNexusCore/DeletePage.xaml.cs
@@ -12,16 +12,34 @@ public partial class DeletePage : ContentPage
     public DeletePage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+    }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+
+        EntryQuery.Text = "";
+        StatusLabel.Text = "Kérlek válaszd ki a törölni kívánt filmet!";
+
+        QueryMovies.Clear();
+
+        SearchQuery(this, EventArgs.Empty);
     }
 
     public async void SearchQuery(Object sender, EventArgs e)
     {
-
+        searchQueryBtn.BackgroundColor = Colors.Orange;
+        QueryMovies.Clear();
+        foreach (var movie in AppData.Movies)
+        {
+            if (movie.title.ToLower().Contains(EntryQuery.Text.ToLower()))
+            {
+                QueryMovies.Add(movie);
+            }
+        }
+        await Task.Delay(1000);
+        searchQueryBtn.BackgroundColor = Color.FromRgb(212, 255, 62);
     }
     public async void deleteMovie(Object sender, EventArgs e)
     {
