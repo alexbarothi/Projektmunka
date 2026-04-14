05a1c1fb1459e9d57b806d39240d6e5170809f4a
alexbarothi <alexbarothi@gmail.com>
2026-04-14 14:10:55 +0200

    Added DeletePageTemplateSelector model and skeleton for DeletePage

--- CritiqlyNexusCore/DeletePage.xaml.cs
+++ CritiqlyNexusCore/DeletePage.xaml.cs
@@ -1,9 +1,59 @@
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Text;
+using CritiqlyNexusCore.Models;
+
 namespace CritiqlyNexusCore;
 
 public partial class DeletePage : ContentPage
 {
-	public DeletePage()
+    public ObservableCollection<Movie> QueryMovies { get; set; } = new ObservableCollection<Movie>();
+    List<Movie> DeletedMovies = new List<Movie>();
+    public DeletePage()
 	{
 		InitializeComponent();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+
+    public async void SearchQuery(Object sender, EventArgs e)
+    {
+
+    }
+    public async void deleteMovie(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void checkDeleted(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void Exit(Object sender, EventArgs e)
+    {
+        var isResponseOk = await DisplayAlertAsync(
+            "Kilépés",
+            "Biztosan ki akarsz lépni? Az eddigi változtatások elvesznek!",
+            "Igen",
+            "Mégse"
+        );
+
+        if (isResponseOk)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public async void Save(Object sender, EventArgs e)
+    {
+
+    }
 }
---
+++ CritiqlyNexusCore/Models/DeletePageTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CritiqlyNexusCore.Models
+{
+    public class DeletePageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate DeletedTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var movie = item as Movie;
+
+            if (movie.IsDeleted)
+                return DeletedTemplate;
+
+            return NormalTemplate;
+        }
+    }
+}
