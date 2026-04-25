224bd5e0cf356065ab7dbc8d212702fc964a1c82
alexbarothi <alexbarothi@gmail.com>
2026-04-25 18:45:58 +0200

    Created StreamingPage, added UI to it and created skeleton in codebehind

---
+++ CritiqlyNexusCore/StreamingPage.xaml.cs
@@ -0,0 +1,59 @@
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json;
+using CritiqlyNexusCore.Models;
+
+namespace CritiqlyNexusCore;
+
+public partial class StreamingPage : ContentPage
+{
+    public StreamingPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
+    public async void VerifyMovie(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void CheckQueve(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void CheckVerified(Object sender, EventArgs e)
+    {
+
+    }
+
+    public async void Exit(Object sender, EventArgs e)
+    {
+        var isResponseOk = await DisplayAlertAsync(
+            "Kilépés",
+            "Biztosan ki akarsz lépni? Az eddigi változtatások elvesznek!",
+            "Igen",
+            "Mégse"
+        );
+
+        if (isResponseOk)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public async void Save(Object sender, EventArgs e)
+    {
+
+    }
+}
