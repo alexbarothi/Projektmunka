cb5cf742356f661980b7552f865dc4f2f4f5d08e
alexbarothi <alexbarothi@gmail.com>
2026-04-26 14:53:34 +0200

    added stramingsubpage ui and codebehind skeleton

--- CritiqlyNexusCore/StreamingSubPage.xaml.cs
+++ CritiqlyNexusCore/StreamingSubPage.xaml.cs
@@ -1,9 +1,30 @@
 namespace CritiqlyNexusCore;
+using CritiqlyNexusCore.Models;
 
 public partial class StreamingSubPage : ContentPage
 {
-	public StreamingSubPage()
-	{
-		InitializeComponent();
-	}
+    public StreamingSubPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+
+    public void LoadSelectedMovie()
+    {
+
+    }
+
+    public async void Save(object sender, EventArgs e)
+    {
+
+    }
+
+    public async void Back(object sender, EventArgs e)
+    {
+
+    }
 }
