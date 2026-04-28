8903f7880175da0015436b0f36ca8a98a8ada3bc
alexbarothi <alexbarothi@gmail.com>
2026-04-28 10:00:25 +0200

    changed installer file

--- CritiqlyNexusCore/MauiProgram.cs
+++ CritiqlyNexusCore/MauiProgram.cs
@@ -37,7 +37,7 @@ namespace CritiqlyNexusCore
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("KimberleyBlRegular", "Kimberley");
+                    fonts.AddFont("KimberleyBlRegular.ttf", "Kimberley");
                 });
 
 #if DEBUG
